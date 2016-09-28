f5502dc83624c7e0eed11de532537ff163b38475
Neil C Smith <(none)>
2016-09-28 15:54:39 +0100

    Example EventBrite listing.

---
+++ js/eventbrite.js
@@ -0,0 +1,26 @@
+(function ($) {
+    "use strict"; // Start of use strict
+    $(document).ready(function () {
+
+        var token = 'FAIJERLIQ6TWYNN52BO4';
+        var $events = $("#events");
+
+        $.get('https://www.eventbriteapi.com/v3/events/search/?token=' + token + '&organizer.id=5862734867&expand=venue', function (res) {
+            console.dir(res);
+            if (res.events.length) {
+                var s = "<ul class='eventList'>";
+                for (var i = 0; i < res.events.length; i++) {
+                    var event = res.events[i];
+                    console.dir(event);
+                    s += "<li><a href='" + event.url + "'>" + event.name.text + "</a> - " + event.description.text + "</li>";
+                }
+                s += "</ul>";
+                $events.html(s);
+            } else {
+                $events.html("<p>Sorry, there are no upcoming events.</p>");
+            }
+        });
+
+    });
+
+})(jQuery); // End of use strict
